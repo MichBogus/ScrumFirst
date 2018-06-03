141498c3407c008c69ac1fe03e905f55cf7625f0
michalbogus <michal.bogus@farmaprom.pl>
2018-06-03 19:55:55 +0200

    First Activity

--- app/src/main/java/com/codefirst/scrumfirst/App.kt
+++ app/src/main/java/com/codefirst/scrumfirst/App.kt
@@ -1,10 +1,19 @@
 package com.codefirst.scrumfirst
 
 import android.support.multidex.MultiDexApplication
+import com.codefirst.scrumfirst.basedi.MainComponent
 
-class App: MultiDexApplication() {
+class App : MultiDexApplication() {
+
+    lateinit var mainComponent: MainComponent
 
     override fun onCreate() {
         super.onCreate()
+
+        mainComponent.inject(this)
+    }
+
+    private fun initComponent() {
+        mainComponent = DaggerMainComponent.create()
     }
 }
