ee98d20c1411e9e2600903dc99cf9aa19431aed2
michalbogus <michal.bogus@farmaprom.pl>
2018-06-03 19:22:38 +0200

    Init of project

---
+++ app/src/main/java/com/codefirst/scrumfirst/App.kt
@@ -0,0 +1,10 @@
+package com.codefirst.scrumfirst
+
+import android.support.multidex.MultiDexApplication
+
+class App: MultiDexApplication() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
